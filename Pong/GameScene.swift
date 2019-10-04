963956b78d258bcaca17b6d1148fa1b2500fa61f
Andy Dymond <andy.dymond@gmail.com>
2019-10-04 12:30:05 -0600

    Make basic unconfigurable pong

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -11,79 +11,82 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+  var ball = SKSpriteNode()
+  var enemyPaddle = SKSpriteNode()
+  var mainPaddle = SKSpriteNode()
+  var ballStartYPosition = CGFloat(0)
+
+  var mainScoreLabel = SKLabelNode()
+  var enemyScoreLabel = SKLabelNode()
+
+  var score = [Int]()
+
+  override func didMove(to view: SKView) {
+    startGame()
+    layoutScene()
+  }
+
+  func startGame() {
+    score = [0, 0]
+  }
+
+  func layoutScene() {
+    self.scaleMode = .aspectFit
+    mainScoreLabel = self.childNode(withName: "mainScoreLabel") as! SKLabelNode
+    enemyScoreLabel = self.childNode(withName: "enemyScoreLabel") as! SKLabelNode
+    ball = self.childNode(withName: "ball") as! SKSpriteNode
+    enemyPaddle = self.childNode(withName: "enemyPaddle") as! SKSpriteNode
+
+    mainPaddle = self.childNode(withName: "mainPaddle") as! SKSpriteNode
+
+    ballStartYPosition = ball.position.y
+    ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+    let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+    border.friction = 0
+    border.restitution = 1
+    self.physicsBody = border
+  }
+
+  func addScore(_ player: SKSpriteNode) {
+    ball.position = CGPoint(x: 0, y: 0)
+    ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+    if player == mainPaddle {
+      score[0] += 1
+      mainScoreLabel.text = "\(score[0])"
+      ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+    } else if player == enemyPaddle {
+      score[1] += 1
+      enemyScoreLabel.text = "\(score[1])"
+      ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for touch in touches {
+      let location = touch.location(in: self)
+      mainPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+  }
+
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if let touch = touches.first {
+      let location = touch.location(in: self)
+      mainPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+  }
+
+  override func update(_ currentTime: TimeInterval) {
+    if ball.position.y <= mainPaddle.position.y - 25 {
+      addScore(enemyPaddle)
+    } else if ball.position.y >= enemyPaddle.position.y + 25 {
+      addScore(mainPaddle)
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+
+    if ball.position.y == ballStartYPosition {
+      let yForce = ballStartYPosition > 0 ? -1 : 1
+      ball.physicsBody?.applyImpulse(CGVector(dx: 0, dy: yForce))
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+    ballStartYPosition = ball.position.y
+    enemyPaddle.run(SKAction.moveTo(x: ball.position.x, duration: 0.5))
+  }
 }
--- Pong/GameViewController.swift
+++ Pong/GameViewController.swift
@@ -12,39 +12,32 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
+    if let view = self.view as! SKView? {
+      if let scene = SKScene(fileNamed: "GameScene") {
+        scene.scaleMode = .aspectFit
+        view.presentScene(scene)
+      }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+      view.ignoresSiblingOrder = true
     }
+  }
 
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+  override var shouldAutorotate: Bool {
+      return true
+  }
+
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+      if UIDevice.current.userInterfaceIdiom == .phone {
+          return .allButUpsideDown
+      } else {
+          return .all
+      }
+  }
+
+  override var prefersStatusBarHidden: Bool {
+      return true
+  }
 }
